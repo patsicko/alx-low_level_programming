9923fe98954ce9ad16549bc1a06d2a8a111e4568
patsicko <patsicko@gmail.com>
2022-10-13 13:00:37 -0700

    fist c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Description: A C program that prints with put function
- *     *
- *      * Return: Always 0 (Success)
- *      */
+ * Description:  A C program that prints with put function
+ */
 
-int main (void)
+int main(void)
 {	
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
