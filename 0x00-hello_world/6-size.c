f80bdefc9ffeadeb5602f49f927058ffd3b5a398
patsicko <patsicko@gmail.com>
2022-10-13 14:24:43 -0700

    indentation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of an double: %lu byte(s)\n", sizeof(double));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));		
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
