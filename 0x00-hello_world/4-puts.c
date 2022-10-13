77d3d957cd8925f69a6cd24d807b44b1df858a88
patsicko <patsicko@gmail.com>
2022-10-13 13:22:41 -0700

    tab

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  * Description:  A C program that prints with put function
  *
  * Return: Always 0 (Success)
+ *
  */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
