666777f9e91075bc484d6c00b500100aefc519c1
patsicko <patsicko@gmail.com>
2022-10-13 12:40:29 -0700

    comments added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
-int main(void)
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Description: A C program that prints with put function
+ *     *
+ *      * Return: Always 0 (Success)
+ *      */
+
+int main (void)
 {	
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 }
