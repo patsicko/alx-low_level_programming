eea41d0011ef8779ea700020261385564ac2124a
patsicko <patsicko@gmail.com>
2022-10-13 14:10:21 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -9,7 +8,6 @@
  */
 int main(void)
 {
-
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
@@ -18,5 +16,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
-
 }
