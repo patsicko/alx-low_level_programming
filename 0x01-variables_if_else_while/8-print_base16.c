cf96113e37b618bb6a24cd7b32abd831c0295709
patsicko <patsicko@gmail.com>
2022-10-14 15:39:10 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print all base 16
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number;
+
+	char letter;
+
+	for (number = 0; number <= 9; number++)
+		putchar(number % 10 + '0');
+
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
+
+	return (0);
+}
