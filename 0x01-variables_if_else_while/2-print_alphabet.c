dfb6f2811e1cf3b7b22ff49da2e2982b784a842f
patsicko <patsicko@gmail.com>
2022-10-14 13:06:54 -0700

    letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *  
+ *  Description: print small letters
+ *
+ *  Return: Always O (Success)
+ */
+int main(void)
+{
+	char letter;
+	
+	for(letter = 'a' ; letter <= 'z' ; letter++ )
+		putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
