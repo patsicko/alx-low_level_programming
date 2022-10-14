cd250fc5da3c9ff1a1f7a72e245b55ac0732600a
patsicko <patsicko@gmail.com>
2022-10-14 12:28:56 -0700

    debug

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
  *
  * Description: print the value of n status:
@@ -14,19 +13,19 @@
  */
 int main(void)
 {
-	int n,digit;
+	int n, digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	/* my codes */
-	digit=n%10;
+	digit = n%10;
 
-	if(digit>5)
+	if (digit > 5)
 	{
 		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
 	}
-	else if(digit==0)
+	else if (digit == 0)
 	{
 		printf("Last digit of %i is %i and is 0\n", n, digit);
 	}
