3573b398d16170c3cc5f2cb2e00fe1dfde3b7ed4
patsicko <patsicko@gmail.com>
2022-10-14 17:43:41 -0700

    100

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,17 +8,13 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 9; a++)
 	{
-		for (b = 1; b < 10; b++)
+		for (b = a + 1; b < 10; b++)
 		{
 			
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-			if (a == 9 && b == 9)
-			{
-				break;
-			}
 
 				putchar(',');
 				putchar(' ');
