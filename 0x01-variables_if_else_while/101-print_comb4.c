7ba16b58367df00eab3bd54a44388f89d2f8cc96
patsicko <patsicko@gmail.com>
2022-10-14 17:31:19 -0700

    comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - Write a program that prints all possible different combinations of three digits.
+ * main - prints all possible different combination.
  *
  * Return: Always 0.
  */
