c817d2e5457e5e70f40c0b77066e985ce57ae33c
patsicko <patsicko@gmail.com>
2022-10-18 13:15:38 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,17 +9,30 @@
  */
 void print_to_98(int n)
 {
-	int count;
+	int i;
+	if (n == 98)
+	{
+		printf("98");
+	}
+	else if (n < 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+			if (i < 98)
+				printf(", ");
+		}
+	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d", i);
+			if (i > 98)
+				printf(", ");
+		}
+	}
 
-	if (n > 98)
-	{
-		for (count = n; count >= 98; count--)
-			printf("%d, ", count);
-	}
-	else
-	{
-		for (count = n; count < 98; count++)
-			printf("%d, ", count);
-		printf("98\n");
-	}
+	printf("\n");
+}
 }
