52879ada7d73c140b5a3137a57b9274093a34ff2
patsicko <patsicko@gmail.com>
2022-10-18 11:31:51 -0700

    table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include"main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; ++num)
+	{
+		_putchar(48);
+		for (mult = 1; mult <= 9; ++mult)
+		{
+		_putchar(',');
+		_putchar(' ');
+
+		prod = num * mult;
+
+		if (prod <= 9)
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar((prod / 10) + 48);
+
+
+			_putchar((prod % 10) + 48);
+		}
+		}
+		_putchar('\n');
+	}
+}
