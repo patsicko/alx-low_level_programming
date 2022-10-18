dc89213d797df4e101acd457c0096e5edb82055d
patsicko <patsicko@gmail.com>
2022-10-18 09:32:18 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,8 +8,12 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122 &&
-			c >= 65 && c >= 90)
-		return (1);
-	return (0);
+	if (c >= 97 && c <= 122 && c >= 65 && c >= 90)
+	{
+		return (0);
+	}
+	else
+	{
+		return (0);
+	}
 }
