77ebf20db8f96a8ee977e9c070afd7b5e353960e
patsicko <patsicko@gmail.com>
2022-10-18 11:35:42 -0700

    table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,16 +19,12 @@ void times_table(void)
 		prod = num * mult;
 
 		if (prod <= 9)
-		{
 			_putchar(' ');
-		}
 		else
-		{
 			_putchar((prod / 10) + 48);
 
 
 			_putchar((prod % 10) + 48);
-		}
 		}
 		_putchar('\n');
 	}
