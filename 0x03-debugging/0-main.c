eb4b758fc93d073e0df936238b83d0b6096ce4e4
patsicko <patsicko@gmail.com>
2022-10-19 08:29:21 -0700

    dbg

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
 #include <main.h>
+
 /**
  * main - tests function that prints if 0 is positive
  *
  * Rturn: Always 0.
  */
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-
 	positive_or_negative(i);
 
 	return (0);
